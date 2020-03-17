457018a86cb1b1baea3065193517d31fc8eefd7c
sx807 <sxing.00789@gmail.com>
2020-03-18 00:12:48 +0800

    update 20200317

--- src/components/Charts/FunList.vue
+++ src/components/Charts/FunList.vue
@@ -1,49 +1,49 @@
 <template>
   <el-table
+    v-loading="loading"
     :data="data.list"
     style="width: 100%"
     height="500"
     max-height="500"
-    v-loading="loading"
-    :default-sort = "{prop: 's_fun', order: 'descending'}"
+    :default-sort="{prop: 's_fun', order: 'descending'}"
   >
     <el-table-column
       type="index"
-      width="50">
-    </el-table-column>
+      width="50"
+    />
     <el-table-column
       prop="s_fun"
       label="源函数"
-      sortable>
-    </el-table-column>
+      sortable
+    />
     <el-table-column
       prop="s_file"
-      label="所在文件">
-    </el-table-column>
+      label="所在文件"
+    />
     <el-table-column
       prop="s_line"
-      label="行号">
-    </el-table-column>
+      label="行号"
+    />
     <el-table-column
       prop="num"
-      label="调用次数">
-    </el-table-column>
+      label="调用次数"
+    />
     <el-table-column
       prop="call_line"
-      label="调用行号">
-    </el-table-column>
+      label="调用行号"
+    />
     <el-table-column
       prop="t_fun"
-      label="被调函数">
-    </el-table-column>
+      label="被调函数"
+    />
     <el-table-column
       prop="t_file"
-      label="所在文件">
-    </el-table-column>
+      label="所在文件"
+    />
     <el-table-column
       prop="t_line"
-      label="行号">
-    </el-table-column>
+      label="行号"
+    />
   </el-table>
 </template>
 
@@ -71,7 +71,7 @@ export default {
   },
   watch: {
     config: {
-      handler(newValue, oldValue) {
+      handler(newValue) {
         console.log(newValue)
         this.getdata()
       },
@@ -101,7 +101,6 @@ export default {
         config[key] = _t.options[key]
       }
       axios.get('http://192.168.3.44:7001/api/v1/functions', { // 还可以直接把参数拼接在url后边
-        // axios.get('./public/data.json', {
         params: config,
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
